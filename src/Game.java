3150f977b1ad8bdba8bd25478e9541931e18a708
22026526-san <sangv6548@gmail.com>
2023-10-13 23:06:03 +0700

    text

--- src/Game.java
+++ src/Game.java
@@ -63,7 +63,7 @@ public class Game {
             guessWord += '-';
         }
         int count = 0;
-        System.out.println("Guess which word is hidden and you have 10 times to guess wrong!");
+        System.out.println("Guess which word is hidden and you have 16 times to guess wrong!");
         System.out.println("Good Luck!! ^^");
         do {
             renderGame(count, guessWord);
@@ -73,11 +73,11 @@ public class Game {
             } else {
                 count++;
             }
-        } while (count <= 10 && !word.equals(guessWord));
+        } while (count <= 16 && !word.equals(guessWord));
 
         renderGame(count, guessWord);
 
-        if (count > 10) {
+        if (count > 16) {
             won = false;
             displayFinalGame(won, word);
         } else {
